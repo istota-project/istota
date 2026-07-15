1bbeb3ea2323021cd143722841561d1033e57db3
Stefan Kubicki <stefan@cynium.com>
2026-07-14 20:35:27 -0700

    Bump version to 0.29.0

--- web/vite-mock-api.ts
+++ web/vite-mock-api.ts
@@ -487,7 +487,7 @@ const chatHandler: MockHandler = ({ url, method, body }) => {
 
 const mockAdminStats = {
 	system: {
-		version: '0.28.0',
+		version: '0.29.0',
 		uptime_seconds: 345600,
 		db_size_bytes: 119447552,
 		python_version: '3.12.3',
